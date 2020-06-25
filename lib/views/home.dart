66a14816da1b3534c661d5e913f75ea877174ba6
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-25 20:46:52 +0530

    Create Quiz view

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -7,6 +7,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final _formKey = GlobalKey<FormState>();
+  String quizImageUrl, quizTitle, quizDescription;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +22,50 @@ class _HomeState extends State<Home> {
         iconTheme: IconThemeData(color: Colors.black87),
         brightness: Brightness.light,
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-             
-          ],
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal:24),
+          child: Column(
+            children: <Widget>[
+               TextFormField(
+                 validator: (val)=> val.isEmpty ? "Enter Image Url":null,
+                 decoration: InputDecoration(
+                   hintText: "Quiz Image Url",
+                 ),
+                 onChanged: (val){
+                   quizImageUrl=val;
+                 },
+               ),
+               SizedBox(height: 6,),
+
+               TextFormField(
+                 validator: (val)=> val.isEmpty ? "Enter Quiz Title":null,
+                 decoration: InputDecoration(
+                   hintText: "Quiz Title",
+                 ),
+                 onChanged: (val){
+                   quizTitle=val;
+                 },
+               ),
+               SizedBox(height: 6,),
+
+               TextFormField(
+                 validator: (val)=> val.isEmpty ? "Quiz Description":null,
+                 decoration: InputDecoration(
+                   hintText: "Quiz Description",
+                 ),
+                 onChanged: (val){
+                   quizDescription=val;
+                 },
+               ),
+               Spacer(),
+
+               blueButton(context, "Create Quiz"),
+               SizedBox(height: 30,),
+
+            ],
+          ),
         ),
       ),
     );
