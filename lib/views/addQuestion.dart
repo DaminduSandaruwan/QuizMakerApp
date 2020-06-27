0fd6a7834c9a3c036d219657a1dbbd6edff2adca
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-27 19:34:29 +0530

    FORM

--- lib/views/addQuestion.dart
+++ lib/views/addQuestion.dart
@@ -20,93 +20,106 @@ class _AddQuestionState extends State<AddQuestion> {
         iconTheme: IconThemeData(color: Colors.black87),
         brightness: Brightness.light,
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal:24),
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              validator: (val) =>
-                  val.isEmpty ? "Enter Question" : null,
-              decoration: InputDecoration(
-                hintText: "Qestion",
-              ),
-              onChanged: (val) {
-                question = val;
-              },
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            TextFormField(
-              validator: (val) =>
-                  val.isEmpty ? "Enter Option 1" : null,
-              decoration: InputDecoration(
-                hintText: "Option 1 (Correct Answer)",
-              ),
-              onChanged: (val) {
-                option1 = val;
-              },
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            TextFormField(
-              validator: (val) =>
-                  val.isEmpty ? "Enter Option 2" : null,
-              decoration: InputDecoration(
-                hintText: "Option 2",
-              ),
-              onChanged: (val) {
-                option2 = val;
-              },
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            TextFormField(
-              validator: (val) =>
-                  val.isEmpty ? "Enter Option 3" : null,
-              decoration: InputDecoration(
-                hintText: "Option 3",
-              ),
-              onChanged: (val) {
-                option3 = val;
-              },
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            TextFormField(
-              validator: (val) =>
-                  val.isEmpty ? "Enter Option 4" : null,
-              decoration: InputDecoration(
-                hintText: "Option 4",
-              ),
-              onChanged: (val) {
-                option4 = val;
-              },
-            ),
-            Spacer(),
-            Row(
-              children: <Widget>[
-                blueButton(
-                  context: context,
-                  label: "Submit",
-                  buttonWidth: MediaQuery.of(context).size.width/2 - 36,
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal:24),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (val) =>
+                    val.isEmpty ? "Enter Question" : null,
+                decoration: InputDecoration(
+                  hintText: "Qestion",
                 ),
-                Spacer(),
-                blueButton(
-                  context: context,
-                  label: "Add Question",
-                  buttonWidth: MediaQuery.of(context).size.width/2 - 36,
+                onChanged: (val) {
+                  question = val;
+                },
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                validator: (val) =>
+                    val.isEmpty ? "Enter Option 1" : null,
+                decoration: InputDecoration(
+                  hintText: "Option 1 (Correct Answer)",
                 ),
-                
-              ],
-            ),
-            SizedBox(
-              height: 25,
-            ),
-          ],
+                onChanged: (val) {
+                  option1 = val;
+                },
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                validator: (val) =>
+                    val.isEmpty ? "Enter Option 2" : null,
+                decoration: InputDecoration(
+                  hintText: "Option 2",
+                ),
+                onChanged: (val) {
+                  option2 = val;
+                },
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                validator: (val) =>
+                    val.isEmpty ? "Enter Option 3" : null,
+                decoration: InputDecoration(
+                  hintText: "Option 3",
+                ),
+                onChanged: (val) {
+                  option3 = val;
+                },
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                validator: (val) =>
+                    val.isEmpty ? "Enter Option 4" : null,
+                decoration: InputDecoration(
+                  hintText: "Option 4",
+                ),
+                onChanged: (val) {
+                  option4 = val;
+                },
+              ),
+              Spacer(),
+              Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: (){
+
+                    },
+                    child: blueButton(
+                      context: context,
+                      label: "Submit",
+                      buttonWidth: MediaQuery.of(context).size.width/2 - 36,
+                    ),
+                  ),
+                  Spacer(),
+                  GestureDetector(
+                    onTap: (){
+                      
+                    },
+                    child: blueButton(
+                      context: context,
+                      label: "Add Question",
+                      buttonWidth: MediaQuery.of(context).size.width/2 - 36,
+                    ),
+                  ),
+                  
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+            ],
+          ),
         ),
       ),
     );
