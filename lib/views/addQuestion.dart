16d21d01c13c5ff8d21ec12471fecea1d2f1db27
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-27 18:14:02 +0530

    Add app bar to add question

--- lib/views/addQuestion.dart
+++ lib/views/addQuestion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_maker/widgets/widgets.dart';
 
 class AddQuestion extends StatefulWidget {
   @override
@@ -8,6 +9,15 @@ class AddQuestion extends StatefulWidget {
 class _AddQuestionState extends State<AddQuestion> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: appBar(context),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.black87),
+        brightness: Brightness.light,
+      ),
+    );
   }
 }
