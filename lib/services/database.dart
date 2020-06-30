c85023e93b5e1ec9bdcfbf00d20ab29338d2ee8b
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-30 19:31:20 +0530

    create function for get question

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -25,4 +25,12 @@ class DatabaseService {
   getQuizData() async {
     return await Firestore.instance.collection("Quiz").snapshots();
   }
+
+  getsQuizData(String quizId) async {
+    return await Firestore.instance
+    .collection("Quiz")
+    .document(quizId)
+    .collection("QNA")
+    .getDocuments();
+  }
 }
