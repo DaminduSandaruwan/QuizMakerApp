56f3bac3ee9695e5c9c00d5fdacbdd77e1826999
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-02 22:37:52 +0530

    Done submit

--- lib/views/result.dart
+++ lib/views/result.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_maker/widgets/widgets.dart';
 
 class Results extends StatefulWidget {
   final int  correct, incorrect,total;
@@ -13,12 +14,39 @@ class _ResultsState extends State<Results> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.all(10),
+        height: MediaQuery.of(context).size.height,
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("${widget.correct}/${widget.total}"),
+              Text(
+                "${widget.correct}/${widget.total}",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
               SizedBox(height: 8,),
-              Text("You answered ${widget.correct} answers correctly and ${widget.incorrect} answers incorrectly")
+              Text(
+                "You answered ${widget.correct} answers correctly and ${widget.incorrect} answers incorrectly",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 14,),
+              GestureDetector(
+                onTap: (){
+                  Navigator.pop(context); 
+                },
+                child: blueButton(
+                  context: context,
+                  label: "Go to Home",
+                  buttonWidth: MediaQuery.of(context).size.width/2,
+                ),
+              ),
             ],
           ),
         ),
